72f1d994553c6a01776256d22cbff2811f4ad400
TakashiSaitou <bookman2710@gmail.com>
2019-03-31 19:10:51 +0900

    ファーストコミット

---
+++ iOSTrainingExample/iOSTrainingExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTrainingExample
+//
+//  Created by 斉藤孝史 on 2019/03/31.
+//  Copyright © 2019 saitoutakashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
